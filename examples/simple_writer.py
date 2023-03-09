bba562e190af8e0dd0507a7e02c5ab76406d2c11
Callum Snowden <10308126+callumsnowden@users.noreply.github.com>
2023-03-09 16:36:27 +0000

    Update simple_writer.py
    Fix issue of always returning success message even if write is unsuccessful.

--- examples/simple_writer.py
+++ examples/simple_writer.py
@@ -9,8 +9,8 @@ DSM_URL = ""
 m = Metrics.Metric(instance=DSM_INSTANCE, key=DSM_KEY, url=DSM_URL)
 for x in range(10):
 	status = m.write({"name":"test_metric", "value":random.randint(1, 50), "label1":"this_label", "label2":"that_label"})
-	if status:
+	if status == True:
 		print("Successfully posted")
-	else:
+	elif status is not True:
 		print("Unsuccessful post, result {}".format(status))
 	time.sleep(1)
